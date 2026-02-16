714fd0b048cc88422e6f9288aa9b6387a45dd830
masatidhira <masatidhira@gmail.com>
2026-02-16 12:17:23 +0700

    feat: add main links data

---
+++ app/utils/Data.tsx
@@ -0,0 +1,6 @@
+export const MainLinks = [
+  { href: "", title: "Beranda" },
+  { href: "tentang", title: "Tentang" },
+  { href: "berita", title: "Berita" },
+  { href: "info", title: "Info" },
+];
