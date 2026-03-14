145572e9b49f8c391cc1f5181884e5bbb3bef547
masatidhira <masatidhira@gmail.com>
2026-03-15 06:51:51 +0700

    fix: change text

--- app/tentang/page.tsx
+++ app/tentang/page.tsx
@@ -1,5 +1,10 @@
 import { Section } from "@/components/ui/Section";
+import { visiMisi } from "@/data/Data";
 
 export default function Tentang() {
-  return <Section>Tentang</Section>;
+  return (
+    <>
+      <Section>TENTANG SEKOLAH</Section>
+    </>
+  );
 }
