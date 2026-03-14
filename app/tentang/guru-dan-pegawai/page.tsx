c651a92c7a56362efdfc853d623a4e762218a618
masatidhira <masatidhira@gmail.com>
2026-03-15 05:08:26 +0700

    feat: initial page content

---
+++ app/tentang/guru-dan-pegawai/page.tsx
@@ -0,0 +1,5 @@
+import { Section } from "@/components/ui/Section";
+
+export default function GuruDanPegawaiPage() {
+  return <Section>GURU</Section>;
+}
---
+++ app/tentang/page.tsx
@@ -0,0 +1,5 @@
+import { Section } from "@/components/ui/Section";
+
+export default function Tentang() {
+  return <Section>Tentang</Section>;
+}
