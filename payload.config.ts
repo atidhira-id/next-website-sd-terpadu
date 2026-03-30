0d1f802c7d8bf8ab1ee0faa4a206b65fd0088880
masatidhira <masatidhira@gmail.com>
2026-03-30 07:33:24 +0700

    feat: add globals

--- payload.config.ts
+++ payload.config.ts
@@ -3,9 +3,11 @@ import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
 import { Media } from "./app/(payload)/collections/Media";
+import { SchoolProfile } from "./app/(payload)/globals/SchoolProfile";
 
 export default buildConfig({
   editor: lexicalEditor(),
+  globals: [SchoolProfile],
   collections: [Media],
   secret: process.env.PAYLOAD_SECRET!,
   db: postgresAdapter({
