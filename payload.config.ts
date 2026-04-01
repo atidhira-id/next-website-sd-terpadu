48ca8c895267028769a72461ae624b0aae494064
masatidhira <masatidhira@gmail.com>
2026-04-01 16:05:46 +0700

    feat: add berita collection

--- payload.config.ts
+++ payload.config.ts
@@ -2,13 +2,14 @@ import sharp from "sharp";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
-import { Media } from "./app/(payload)/collections/Media";
 import { SchoolProfile } from "./app/(payload)/globals/SchoolProfile";
+import Media from "./app/(payload)/collections/Media";
+import Berita from "./app/(payload)/collections/Berita";
 
 export default buildConfig({
   editor: lexicalEditor(),
   globals: [SchoolProfile],
-  collections: [Media],
+  collections: [Media, Berita],
   secret: process.env.PAYLOAD_SECRET!,
   db: postgresAdapter({
     pool: {
