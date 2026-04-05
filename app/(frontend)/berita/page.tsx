6adfef6c20a5bdd89433b53a227f6ab5b179efac
masatidhira <masatidhira@gmail.com>
2026-04-05 23:04:43 +0700

    feat: update page content

--- app/(frontend)/berita/page.tsx
+++ app/(frontend)/berita/page.tsx
@@ -1,9 +1,101 @@
-import { Section } from "@/components/ui/Section";
+import { Section, SectionTitle } from "@/components/ui/Section";
+import { getPayloadClient } from "@/lib/payload";
+import { Badge } from "@/components/shadcn/badge";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/shadcn/card";
+import Link from "next/link";
+import { getDateString } from "@/utils/dateFormat";
+import { ArrowRight } from "lucide-react";
 
-export default function Tentang() {
+export default async function ListBerita() {
+  const payload = await getPayloadClient();
+  const { docs: beritaList } = await payload.find({
+    collection: "berita",
+    where: { status: { equals: "published" } },
+    sort: "-tanggalPublikasi",
+  });
+
+  const { docs: beritaTerbaru } = await payload.find({
+    collection: "berita",
+    where: { status: { equals: "published" } },
+    sort: "-tanggalPublikasi",
+    limit: 3,
+  });
+
   return (
     <>
       <Section>BERITA TERBARU</Section>
+      <Section>
+        <div className="grid lg:grid-cols-3 gap-16">
+          <div className="lg:col-span-2 not-lg:order-2 ">
+            {beritaList.map((berita) => (
+              <Card
+                className="relative bg-card mx-auto mb-4 w-full pt-0 rounded-none border-none shadow-none overflow-hidden"
+                key={berita.id}
+              >
+                <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+                <img
+                  src={berita.imageUrl.url}
+                  alt="Gamber Berita"
+                  className="relative z-20 aspect-video w-full object-cover brightness-80 grayscale dark:brightness-40"
+                />
+                <CardHeader>
+                  <CardAction>
+                    <Badge>{getDateString(berita.tanggalPublikasi)}</Badge>
+                  </CardAction>
+                  <CardTitle className="text-xl">{berita.judul}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <CardDescription>{berita.ringkasan}</CardDescription>
+                </CardContent>
+                <CardFooter className="flex justify-end-safe">
+                  <Link
+                    href={`/berita/${berita.slug}`}
+                    className="flex items-center gap-1 italic hover:underline"
+                  >
+                    <p>Selengkapnya</p> <ArrowRight size={18} />
+                  </Link>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
+          <div className="not-lg:order-1">
+            <SectionTitle>Berita Terbaru</SectionTitle>
+            {beritaTerbaru.map((berita) => (
+              <Card
+                className="relative bg-inherit mx-auto mb-8 p-0 w-full flex flex-row gap-4 rounded-none border-none shadow-none"
+                key={berita.id}
+              >
+                <div className="flex-1/4">
+                  <img
+                    src={berita.imageUrl.url}
+                    alt="Gamber Berita"
+                    className="relative z-20 aspect-square object-cover"
+                  />
+                </div>
+                <CardDescription className="flex-3/4 flex flex-col">
+                  <Link href={`/berita/${berita.slug}`}>
+                    <CardTitle className="not-lg:text-[1.2rem] lg:text-[0.8rem] xl:text-[1rem] hover:text-black">
+                      {berita.judul}
+                    </CardTitle>
+                  </Link>
+                  <div className="flex gap-4 mt-2 text-xs uppercase">
+                    <p>{getDateString(berita.tanggalPublikasi)}</p>
+                    <p>{berita.penulis}</p>
+                  </div>
+                </CardDescription>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </Section>
     </>
   );
 }
