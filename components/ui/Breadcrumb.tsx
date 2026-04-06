fba48d6ac3cd1a1c1729ee5f7164bf437d4d727c
masatidhira <masatidhira@gmail.com>
2026-04-06 08:40:54 +0700

    feat: add breadcrumb component

---
+++ components/ui/Breadcrumb.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/shadcn/breadcrumb";
+
+type BreadcrumbItemType = {
+  label: string;
+  href?: string;
+};
+
+type BreadcrumbMenuProps = {
+  items: BreadcrumbItemType[];
+};
+
+export function BreadcrumbMenu({ items }: BreadcrumbMenuProps) {
+  return (
+    <Breadcrumb className="mb-8">
+      <BreadcrumbList className="text-md">
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">SD Terpadu Muhammadiyah 1 Besuki</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        {items.map((item, index) => {
+          const isLast = index === items.length - 1;
+          return (
+            <div key={index} className="flex items-center">
+              <BreadcrumbItem>
+                {isLast || !item.href ? (
+                  <BreadcrumbPage>{item.label}</BreadcrumbPage>
+                ) : (
+                  <BreadcrumbLink asChild>
+                    <Link href={item.href}>{item.label}</Link>
+                  </BreadcrumbLink>
+                )}
+              </BreadcrumbItem>
+              {!isLast && <BreadcrumbSeparator />}
+            </div>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
