e994b062305375687c3a945d95299f2026749b64
masatidhira <masatidhira@gmail.com>
2026-04-08 11:24:41 +0700

    feat: add jabatan and guru collection

--- payload.config.ts
+++ payload.config.ts
@@ -5,11 +5,13 @@ import { buildConfig } from "payload";
 import { SchoolProfile } from "./app/(payload)/globals/SchoolProfile";
 import Media from "./app/(payload)/collections/Media";
 import Berita from "./app/(payload)/collections/Berita";
+import Guru from "./app/(payload)/collections/Guru";
+import Jabatan from "./app/(payload)/collections/Jabatan";
 
 export default buildConfig({
   editor: lexicalEditor(),
   globals: [SchoolProfile],
-  collections: [Media, Berita],
+  collections: [Media, Berita, Jabatan, Guru],
   secret: process.env.PAYLOAD_SECRET!,
   db: postgresAdapter({
     pool: {
