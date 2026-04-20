e1b7e80e2d72726c99114531be9f550e17f9eee2
masatidhira <masatidhira@gmail.com>
2026-04-21 01:28:17 +0700

    fix: handle changes in db, jabatan now can have more than one guru

--- app/(frontend)/tentang/guru-dan-pegawai/page.tsx
+++ app/(frontend)/tentang/guru-dan-pegawai/page.tsx
@@ -1,7 +1,7 @@
 import { BreadcrumbMenu } from "@/components/ui/Breadcrumb";
 import { PageTitleSection, Section } from "@/components/ui/Section";
 import { getPayloadClient } from "@/lib/payload";
-import Image from "next/image";
+import GuruCard from "@/components/ui/GuruCard";
 
 export default async function GuruDanPegawaiPage() {
   const breadcrumbItems = [
@@ -23,30 +23,19 @@ export default async function GuruDanPegawaiPage() {
       <PageTitleSection title="Daftar Guru dan Pegawai" />
       <Section containerClassName="pt-4 md:pt-6">
         <BreadcrumbMenu items={breadcrumbItems} />
-
-        <ul className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-y-12">
-          {jabatanList.map((jabatan) => (
-            <li
-              key={jabatan.nama}
-              className="mx-auto w-64 h-96 flex flex-col rounded-tr-2xl rounded-bl-2xl overflow-hidden text-primary"
-            >
-              <div className="bg-secondary-foreground p-2 tracking-wider">
-                {jabatan.nama}
-              </div>
-              <div className="relative w-full h-full">
-                <Image
-                  src={jabatan.guru.foto.url}
-                  alt="Foto guru"
-                  fill
-                  className="object-cover"
-                />
-              </div>
-              <div className="bg-secondary-foreground p-2 text-right">
-                {jabatan.guru.nama}
-              </div>
-            </li>
-          ))}
-        </ul>
+        {jabatanList && (
+          <ul className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-y-12">
+            {jabatanList.map((jabatan) => (
+              <li
+                key={jabatan.nama}
+                className="mx-auto w-64 h-96 flex flex-col rounded-tr-2xl rounded-bl-2xl overflow-hidden text-primary"
+              >
+                <GuruCard data={jabatan} />
+              </li>
+            ))}
+          </ul>
+        )}
+        {!jabatanList && <div>Belum ada data</div>}
       </Section>
     </>
   );
