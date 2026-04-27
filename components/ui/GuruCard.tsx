0b44395d73e2730d8d70b724b3e6c38ea018cc0f
masatidhira <masatidhira@gmail.com>
2026-04-28 06:54:10 +0700

    fix: change props to always single guru data

--- components/ui/GuruCard.tsx
+++ components/ui/GuruCard.tsx
@@ -4,26 +4,21 @@ interface GuruCardProps {
 }
 
 export default function GuruCard({ data }: GuruCardProps) {
-  const namaJabatan = data.nama;
-  const guruList = data.guru;
-
   return (
     <>
       <div className="bg-secondary-foreground p-2 tracking-wide uppercase font-bold">
-        {namaJabatan}
+        {data.jenis}
       </div>
       <div className="relative w-full h-full bg-gray-200">
-        {guruList.length == 1 && (
-          <Image
-            src={guruList[0].foto.url}
-            alt="Foto guru"
-            fill
-            className="object-cover"
-          />
-        )}
+        <Image
+          src={data.foto.url}
+          alt="Foto guru"
+          fill
+          className="object-cover"
+        />
       </div>
       <div className="bg-secondary-foreground p-2 text-right tracking-wide">
-        {guruList.length == 1 ? guruList[0].nama : "Kosong"}
+        {data.nama}
       </div>
     </>
   );
