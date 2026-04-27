333520b733f8a360b73fb1f973c4733b342d7c50
masatidhira <masatidhira@gmail.com>
2026-04-28 06:55:34 +0700

    feat: seperate guru and pegawai list

--- app/(frontend)/tentang/guru-dan-pegawai/page.tsx
+++ app/(frontend)/tentang/guru-dan-pegawai/page.tsx
@@ -1,5 +1,9 @@
 import { BreadcrumbMenu } from "@/components/ui/Breadcrumb";
-import { PageTitleSection, Section } from "@/components/ui/Section";
+import {
+  PageTitleSection,
+  Section,
+  SectionTitle,
+} from "@/components/ui/Section";
 import { getPayloadClient } from "@/lib/payload";
 import GuruCard from "@/components/ui/GuruCard";
 
@@ -11,31 +15,51 @@ export default async function GuruDanPegawaiPage() {
 
   const payload = await getPayloadClient();
 
-  const { docs: jabatanList } = await payload.find({
-    collection: "jabatan",
-    sort: "urutan",
+  const { docs: guruList } = await payload.find({
+    collection: "guru",
+    sort: "jenis",
     limit: 0,
     depth: 2,
   });
+
+  const daftarGuru = guruList.filter((g) => g.jenis === "guru");
+  const daftarPegawai = guruList.filter((g) => g.jenis === "pegawai");
 
   return (
     <>
       <PageTitleSection title="Daftar Guru dan Pegawai" />
       <Section containerClassName="pt-4 md:pt-6">
         <BreadcrumbMenu items={breadcrumbItems} />
-        {jabatanList && (
+        <SectionTitle className="mb-8">Daftar Guru</SectionTitle>
+        {daftarGuru && (
           <ul className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-y-12">
-            {jabatanList.map((jabatan) => (
+            {daftarGuru.map((guru) => (
               <li
-                key={jabatan.nama}
+                key={guru.nama}
                 className="mx-auto w-64 h-96 flex flex-col rounded-tr-2xl rounded-bl-2xl overflow-hidden text-primary"
               >
-                <GuruCard data={jabatan} />
+                <GuruCard data={guru} />
               </li>
             ))}
           </ul>
         )}
-        {!jabatanList && <div>Belum ada data</div>}
+      </Section>
+      <Section>
+        {daftarPegawai.length > 0 && (
+          <>
+            <SectionTitle className="mb-8">Daftar Pegawai</SectionTitle>
+            <ul className="grid md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-y-12">
+              {daftarPegawai.map((pegawai) => (
+                <li
+                  key={pegawai.nama}
+                  className="mx-auto w-64 h-96 flex flex-col rounded-tr-2xl rounded-bl-2xl overflow-hidden text-primary"
+                >
+                  <GuruCard data={pegawai} />
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </Section>
     </>
   );
