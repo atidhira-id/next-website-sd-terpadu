5e79765dec2aaf1ece38c500266478d62b4cfe99
masatidhira <masatidhira@gmail.com>
2026-04-28 06:55:57 +0700

    feat: add sambutan kepala sekolah

--- app/(frontend)/tentang/page.tsx
+++ app/(frontend)/tentang/page.tsx
@@ -1,16 +1,56 @@
+"use client";
+import { useSchool } from "@/context/schoolContext";
+import {
+  RichText,
+  defaultJSXConverters,
+  type JSXConverters,
+} from "@payloadcms/richtext-lexical/react";
 import { BreadcrumbMenu } from "@/components/ui/Breadcrumb";
-import { PageTitleSection, Section } from "@/components/ui/Section";
+import {
+  PageTitleSection,
+  Section,
+  SectionTitle,
+} from "@/components/ui/Section";
+import Image from "next/image";
 
 export default function Tentang() {
+  const schoolProfile = useSchool();
+
   return (
     <>
       <PageTitleSection title="Tentang Sekolah" />
       <Section containerClassName="pt-4 md:pt-6">
         <BreadcrumbMenu items={[{ label: "Tentang" }]} />
-        <h3 className="text-black font-bold text-center uppercase text-2xl">
-          Halaman belum tersedia
-        </h3>
+        <SectionTitle className="mt-16 mb-8">
+          Sambutan Kepala Sekolah
+        </SectionTitle>
+        <div className="w-full grid md:grid-cols-2 text-foreground gap-y-8">
+          <div className="relative min-h-96">
+            <Image
+              src={schoolProfile.fotoKepalaSekolah.url}
+              alt="Foto Kepala Sekolah"
+              fill
+              className="object-contain"
+            />
+          </div>
+          <div>
+            <RichText
+              data={schoolProfile.sambutanKepalaSekolah}
+              converters={converters}
+            />
+          </div>
+        </div>
+        <SectionTitle className="mt-16 mb-8">Struktur Organisasi</SectionTitle>
       </Section>
     </>
   );
 }
+
+const converters: JSXConverters = {
+  ...defaultJSXConverters,
+  paragraph: ({ node, nodesToJSX }) => {
+    const children = nodesToJSX({ nodes: node.children });
+
+    return <p className="mb-4 leading-relaxed">{children}</p>;
+  },
+};
